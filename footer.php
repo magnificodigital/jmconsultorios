a85e245ab3d9b70fc370ec0b11dde8f4e466ac80
Caio Cesar <caio@magnificodigital.com>
2017-06-07 11:49:36 -0300

    Atualizacao codigo analytics

--- footer.php
+++ footer.php
@@ -236,13 +236,13 @@
 
 
 <!--Google Analytics-->
-<script async>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-93182334-1', 'auto');
+  ga('create', 'UA-100622361-1', 'auto');
   ga('send', 'pageview');
 </script>
 
